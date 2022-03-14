c509fb3ce2b086202762b0dc411881dedc18a532
Aayush Swodari <66193594+Aayushdn@users.noreply.github.com>
2022-03-14 23:06:07 +0545

    added download functionality and some animation

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const allNavLinks = document.querySelectorAll(".navLink");
-const navLinks = document.querySelector(".navLinks");
+const allNavLinks = document.querySelectorAll('.navLink');
+const navLinks = document.querySelector('.navLinks');
+const skillSection = document.querySelector('.skillsSection');
+const projects = document.querySelectorAll('.projectCard');
+const experiences = document.querySelectorAll('.event');
+const cvBtn = document.querySelector('.cta--primary');
+
+console.log(experiences);
 
 // alert("hello world");
 console.log(navLinks);
 
-navLinks.addEventListener("click", (e) => {
+const observer = new IntersectionObserver(
+  (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting) entry.target.classList.add('visible');
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+projects.forEach((project) => {
+  observer.observe(project);
+});
+
+experiences.forEach((experience) => {
+  observer.observe(experience);
+});
+
+cvBtn.addEventListener('click', function () {
+  document.querySelector('.downloadFile').click();
+});
+
+navLinks.addEventListener('click', (e) => {
   let target = e.target;
   console.log(target.classList);
   let section;
-  if (target.classList.contains("navLink")) {
+  if (target.classList.contains('navLink')) {
     section = `.${target.dataset.section}`;
     console.log(section);
-    document.querySelector(section).scrollIntoView({ behavior: "smooth" });
+    document.querySelector(section).scrollIntoView({ behavior: 'smooth' });
   }
 });
